d7b75fea6be7cebc79c99037cdc0468caf40d14a
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2023-04-15 15:12:31 +0300

    Light and ground.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// Deps
+// npx create-react-app . --template minimal
+// npm install three @react-three/fiber @react-three/drei @react-three/postprocessing
